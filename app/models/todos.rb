730e0182fdc87806bf48555b125a11630489d17f
Suvarna <srajkumar_nyros@yahoo.com>
2014-09-26 09:08:06 +0530

    heroku push

--- app/models/todos.rb
+++
@@ -1,4 +0,0 @@
-class Todos < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
-	belongs_to :user
-end
--- db/migrate/20140924071613_add_fields_to_todos.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldsToTodos < ActiveRecord::Migration
-  def change
-    add_column :todos, :user_id, :integer
-  end
-end
